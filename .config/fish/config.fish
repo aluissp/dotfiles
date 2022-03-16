3f3ec552a62095941ed4ba7126d56a39630352c2
luis <luisprg05@gmail.com>
2022-03-15 22:53:56 -0500

    Agregado alias pgstart, pgstop y pgadmin en fish...

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,9 @@ alias grep "grep --color=auto"
 alias cat "bat --style=plain --paging=never"
 alias ls "exa --group-directories-first"
 alias tree "exa -T"
+alias pgstart "sudo systemctl start postgresql.service"
+alias pgstop "sudo systemctl stop postgresql.service"
+alias pgadmin "source pgadmin4/bin/activate.fish"
 # alias dotfiles "git --git-dir $HOME/.dotfiles/ --work-tree $HOME"
 
 
