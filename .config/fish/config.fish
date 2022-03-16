63a6c5d8af923afc3b19a59ad625c41770b3c6b0
luis <luisprg05@gmail.com>
2022-03-15 21:38:48 -0500

    Corregido el respaldo...

---
+++ .config/fish/config.fish
@@ -0,0 +1,15 @@
+# Greeting
+set fish_greeting ""
+neofetch
+
+# Aliases
+
+alias grep "grep --color=auto"
+alias cat "bat --style=plain --paging=never"
+alias ls "exa --group-directories-first"
+alias tree "exa -T"
+# alias dotfiles "git --git-dir $HOME/.dotfiles/ --work-tree $HOME"
+
+
+# Prompt
+starship init fish | source
