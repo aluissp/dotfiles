8b5e96b1a3f1ead2ddc2cd3075f258c99b094403
luis <luisprg05@gmail.com>
2022-07-14 21:29:02 -0500

    Alias para mysql y apache añadido!

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,6 +11,12 @@ alias tree "exa -T"
 alias pgstart "sudo systemctl start postgresql.service"
 alias pgstop "sudo systemctl stop postgresql.service"
 alias pgadmin "source pgadmin4/bin/activate.fish"
+alias mysqlstatus "sudo systemctl status mysqld.service"
+alias mysqlstart "sudo systemctl start mysqld.service"
+alias mysqlstop "sudo systemctl stop mysqld.service"
+alias apachestatus "sudo systemctl status httpd.service"
+alias apachestart "sudo systemctl start httpd.service"
+alias apachestop "sudo systemctl stop httpd.service"
 # alias dotfiles "git --git-dir $HOME/.dotfiles/ --work-tree $HOME"
 
 
